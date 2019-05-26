8c3db60a178ef36fb2b0703a8a5df655c97629a0
David Whynot <davidmwhynot@gmail.com>
2019-05-26 16:35:56 -0400

    refactored to use injector function

--- gulpfile.js
+++ gulpfile.js
@@ -24,23 +24,7 @@ marked.setOptions({
 	smartLists: true,
 	smartypants: false,
 	xhtml: false,
-	highlight: (code, language) => {
-		console.log('language');
-		console.log(language);
-
-		const highlightedCode = highlight.highlight(language, code);
-
-		console.log('highlightedCode.language');
-		console.log(highlightedCode.language);
-
-		// console.log('highlightedCode.relevance');
-		// console.log(highlightedCode.relevance);
-
-		console.log('highlightedCode.value');
-		console.log(highlightedCode.value);
-
-		return highlightedCode.value;
-	}
+	highlight: (code, language) => highlight.highlight(language, code).value
 });
 
 const isDirectory = source => lstatSync(source).isDirectory();
@@ -49,6 +33,32 @@ const getDirectories = source =>
 	readdirSync(source)
 		.map(name => join(source, name))
 		.filter(isDirectory);
+
+// injects a number of key-value pairs into a template file using inject-string
+// @param template - the template string
+// @param fields - an object where the property names should match the tag inside the template string where the value of said properties should be stored
+// @returns - a string with the injected fields
+const injector = (template, fields) => {
+	// get tags from object keys in fields object
+	const tags = Object.keys(fields);
+
+	for (const tag of tags) {
+		template = inject(template, tag, fields[tag]);
+	}
+
+	return template;
+};
+
+const inject = (template, tag, value) => {
+	const injectObject = new InjectString(template, {
+		stripTags: true,
+		tag: tag,
+		delimiters: ['{{{', '}}}'],
+		newlines: false
+	});
+
+	return injectObject.replace(value);
+};
 
 // TASK: index
 gulp.task('index', done => {
@@ -68,32 +78,13 @@ gulp.task('index', done => {
 			'utf8'
 		);
 
-		const topicLink = new InjectString(topicTemplate, {
-			stripTags: true,
-			tag: 'link',
-			delimiters: ['{{{', '}}}'],
-			newlines: false
+		const topicLink = topicDirName + '/index.html';
+
+		const generatedTopic = injector(topicTemplate, {
+			link: topicLink,
+			title: meta.title,
+			description: meta.description
 		});
-
-		let generatedTopic = topicLink.replace(topicDirName + '/index.html');
-
-		const topicTitle = new InjectString(generatedTopic, {
-			stripTags: true,
-			tag: 'title',
-			delimiters: ['{{{', '}}}'],
-			newlines: false
-		});
-
-		generatedTopic = topicTitle.replace(meta.title);
-
-		const topicDescription = new InjectString(generatedTopic, {
-			stripTags: true,
-			tag: 'description',
-			delimiters: ['{{{', '}}}'],
-			newlines: false
-		});
-
-		generatedTopic = topicDescription.replace(meta.description);
 
 		topics.push({
 			output: generatedTopic,
@@ -151,34 +142,13 @@ gulp.task('topics', done => {
 				'utf8'
 			);
 
-			const articleLink = new InjectString(articleTemplate, {
-				stripTags: true,
-				tag: 'link',
-				delimiters: ['{{{', '}}}'],
-				newlines: false
+			const articleLink = articleDirName + '/' + meta.pages[0] + '.html';
+
+			const generatedArticle = injector(articleTemplate, {
+				link: articleLink,
+				title: meta.title,
+				description: meta.description
 			});
-
-			let generatedArticle = articleLink.replace(
-				articleDirName + '/' + meta.pages[0] + '.html'
-			);
-
-			const articleTitle = new InjectString(generatedArticle, {
-				stripTags: true,
-				tag: 'title',
-				delimiters: ['{{{', '}}}'],
-				newlines: false
-			});
-
-			generatedArticle = articleTitle.replace(meta.title);
-
-			const articleDescription = new InjectString(generatedArticle, {
-				stripTags: true,
-				tag: 'description',
-				delimiters: ['{{{', '}}}'],
-				newlines: false
-			});
-
-			generatedArticle = articleDescription.replace(meta.description);
 
 			articles.push({
 				output: generatedArticle,
@@ -189,41 +159,24 @@ gulp.task('topics', done => {
 			});
 		}
 
-		let breadcrumb = readFileSync(
+		const breadcrumbTemplate = readFileSync(
 			'./src/templates/topic/breadcrumb.html',
 			'utf8'
 		);
 
-		console.log(breadcrumb);
-
-		let breadcrumbInjectTitle = new InjectString(breadcrumb, {
-			stripTags: true,
-			tag: 'title',
-			delimiters: ['{{{', '}}}'],
-			newlines: false
+		const breadcrumb = injector(breadcrumbTemplate, {
+			title: topicMeta.title
 		});
 
-		breadcrumb = breadcrumbInjectTitle.replace(topicMeta.title);
+		let headerTemplate = readFileSync(
+			'./src/templates/topic/header.html',
+			'utf8'
+		);
 
-		let output = readFileSync('./src/templates/topic/header.html', 'utf8');
-
-		let outputInjectBreadcrumb = new InjectString(output, {
-			stripTags: true,
-			tag: 'breadcrumb',
-			delimiters: ['{{{', '}}}'],
-			newlines: false
+		let output = injector(headerTemplate, {
+			breadcrumb: breadcrumb,
+			title: topicMeta.title
 		});
-
-		output = outputInjectBreadcrumb.replace(breadcrumb);
-
-		let outputInjectTitle = new InjectString(output, {
-			stripTags: true,
-			tag: 'title',
-			delimiters: ['{{{', '}}}'],
-			newlines: false
-		});
-
-		output = outputInjectTitle.replace(topicMeta.title);
 
 		for (let article of articles) {
 			output += article.output;
@@ -264,95 +217,10 @@ gulp.task('articles', done => {
 
 			const articleDirName = article.match(/\w+?$/g)[0];
 
-			const pageTemplate = readFileSync(
-				'./src/templates/article/page.html',
-				'utf8'
-			);
-
 			for (let page of meta.pages) {
-				// parse markdwon to html
-
-				let breadcrumb = readFileSync(
-					'./src/templates/article/breadcrumb.html',
-					'utf8'
-				);
-
-				let breadcrumbInjectTopicLink = new InjectString(breadcrumb, {
-					stripTags: true,
-					tag: 'topic-link',
-					delimiters: ['{{{', '}}}'],
-					newlines: false
-				});
-				breadcrumb = breadcrumbInjectTopicLink.replace('/' + topicDirName);
-
-				let breadcrumbInjectTopicTitle = new InjectString(breadcrumb, {
-					stripTags: true,
-					tag: 'topic-title',
-					delimiters: ['{{{', '}}}'],
-					newlines: false
-				});
-				breadcrumb = breadcrumbInjectTopicTitle.replace(topicMeta.title);
-
-				let breadcrumbInjectArticleTitle = new InjectString(breadcrumb, {
-					stripTags: true,
-					tag: 'article-title',
-					delimiters: ['{{{', '}}}'],
-					newlines: false
-				});
-				breadcrumb = breadcrumbInjectArticleTitle.replace(meta.title);
-
-				console.log(breadcrumb);
-
-				let output = readFileSync(
-					'./src/templates/article/header.html',
-					'utf8'
-				);
-
-				let outputInjectBreadcrumb = new InjectString(output, {
-					stripTags: true,
-					tag: 'breadcrumb',
-					delimiters: ['{{{', '}}}'],
-					newlines: false
-				});
-				output = outputInjectBreadcrumb.replace(breadcrumb);
-
-				// parse title
-				const pageTitle = titleCase(page.replace(/-/g, ' '));
-
-				let pageTemplateInjectTitle = new InjectString(pageTemplate, {
-					stripTags: true,
-					tag: 'title',
-					delimiters: ['{{{', '}}}'],
-					newlines: false
-				});
-				let content = pageTemplateInjectTitle.replace(pageTitle);
-
-				// get page markdown
-				const pageMarkdown = readFileSync(
-					'./content/topics/' +
-						topicDirName +
-						'/' +
-						articleDirName +
-						'/' +
-						page +
-						'.md',
-					'utf8'
-				);
-
-				// parse page markdown
-				const markdown = marked(pageMarkdown);
-
-				// inject markdown
-				let pageTemplateInjectMarkdown = new InjectString(content, {
-					stripTags: true,
-					tag: 'markdown',
-					delimiters: ['{{{', '}}}'],
-					newlines: false
-				});
-				content = pageTemplateInjectMarkdown.replace(markdown);
-
 				// construct page navigation
 				let outputNav = ' ';
+
 				// do we need a navigation? articles with one page do not need a navigation...
 				if (meta.pages.length > 1) {
 					// get page nav template
@@ -385,56 +253,91 @@ gulp.task('articles', done => {
 						const navPageTitle = titleCase(navPage.replace(/-/g, ' '));
 
 						// initialize nav item active
-						let navPageActive = '';
+						let navPageCurrent = '';
 
 						// set nav item active
-						if (page === navPage) navPageActive = 'active';
+						if (page === navPage) navPageCurrent = 'disabled';
+						console.log('page:');
+						console.log(page);
+						console.log('navPage:');
+						console.log(navPage);
+						console.log('navPageCurrent:');
+						console.log(navPageCurrent);
 
-						// inject values
-						let navItem = injector({
-							source: navItemTemplate,
-							input: navPageActive,
-							tag: 'active'
-						});
-						navItem = injector({
-							source: navItem,
-							input: navPageLink,
-							tag: 'link'
-						});
-						navItem = injector({
-							source: navItem,
-							input: navPageTitle,
-							tag: 'title'
+						let navItem = injector(navItemTemplate, {
+							current: navPageCurrent,
+							link: navPageLink,
+							title: navPageTitle
 						});
 
 						navItems += navItem;
 					}
 
 					// add nav items to nav
-					outputNav = injector({
-						source: navTemplate,
-						input: navItems,
-						tag: 'items'
+					outputNav = injector(navTemplate, {
+						items: navItems
 					});
 				}
 
-				console.log('outputNav');
-				console.log(outputNav);
-				console.log('content');
-				console.log(content);
+				// get page markdown
+				const pageMarkdown = readFileSync(
+					'./content/topics/' +
+						topicDirName +
+						'/' +
+						articleDirName +
+						'/' +
+						page +
+						'.md',
+					'utf8'
+				);
 
-				// inject nav
-				let pageTemplateInjectNav = new InjectString(content, {
-					stripTags: true,
-					tag: 'nav',
-					delimiters: ['{{{', '}}}'],
-					newlines: false
+				// parse page markdown
+				const markdown = marked(pageMarkdown);
+
+				// parse page title
+				const pageTitle = titleCase(page.replace(/-/g, ' '));
+
+				// generate page content from page template
+				const pageTemplate = readFileSync(
+					'./src/templates/article/page.html',
+					'utf8'
+				);
+
+				// inject tags into content
+				const content = injector(pageTemplate, {
+					nav: outputNav,
+					title: pageTitle,
+					markdown: markdown
 				});
-				content = pageTemplateInjectNav.replace(outputNav);
 
-				// add page content to nav
+				// generate breadcrumb
+				const breadcrumbTemplate = readFileSync(
+					'./src/templates/article/breadcrumb.html',
+					'utf8'
+				);
+
+				const breadcrumbTopicLink = '/' + topicDirName;
+
+				const breadcrumb = injector(breadcrumbTemplate, {
+					topicLink: breadcrumbTopicLink,
+					topicTitle: topicMeta.title,
+					articleTitle: meta.title
+				});
+
+				// generate output starting with header template
+				const headerTemplate = readFileSync(
+					'./src/templates/article/header.html',
+					'utf8'
+				);
+
+				let output = injector(headerTemplate, {
+					breadcrumb: breadcrumb
+				});
+
+				// add page content to output
 				output += content;
 
+				// add footer to output
 				output += readFileSync('./src/templates/article/footer.html', 'utf8');
 
 				mkdirSync('./dist/' + topicDirName + '/' + articleDirName, {
@@ -457,31 +360,5 @@ gulp.task('articles', done => {
 
 	done();
 });
-
-// injects a number of key-value pairs into a template file using inject-string
-// @param template - the template string
-// @param fields - an object where the property names should match the tag inside the template string where the value of said properties should be stored
-// @returns - a string with the injected fields
-function injector(template, fields) {
-	// get tags from object keys in fields object
-	const tags = Object.keys(fields);
-
-	for (const tag of tags) {
-		template = inject(template, tag, fields[tag]);
-	}
-
-	return template;
-}
-
-function inject(template, tag, value) {
-	const injectObject = new InjectString(template, {
-		stripTags: true,
-		tag: tag,
-		delimiters: ['{{{', '}}}'],
-		newlines: false
-	});
-
-	return injectObject.replace(value);
-}
 
 gulp.task('build', gulp.series('index', 'topics', 'articles'));
